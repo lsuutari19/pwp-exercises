473a578840e87de85224f9af46742e022d4b3f6f
[suturri] <lsuutari19@student.oulu.fi>
2023-02-22 15:41:30 +0200

    exercise 2 task 1 completed and started initial exercise structure

---
+++ exc2/producthub/resources/product.py
@@ -0,0 +1,77 @@
+import sqlite3
+from flask import Flask, request, g
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+api = Api(app)
+
+app.app_context().push()
+
+
+class ProductCollection(Resource):
+
+    def get(self):
+        products = Product.query.all()
+        items = []
+        for product in products:
+            item = {
+                "handle": product.handle,
+                "weight": product.weight,
+                "price": product.price
+            }
+            items.append(item)
+        return items, 200
+
+    def post(self):
+        try:
+            handle = request.json["handle"]
+            weight = request.json["weight"]
+            price = request.json["price"]
+        except KeyError:
+            return "Incomplete request - missing fields", 400
+        try:
+            float(weight)
+            float(price)
+        except ValueError:
+            return "Weight and price must be numbers", 400
+
+        product_entry = Product(
+            handle=handle,
+            weight=weight,
+            price=price,
+        )
+        try:
+            db.session.add(product_entry)
+            db.session.commit()
+        except IntegrityError:
+            return "Handle already exists", 409
+        return "", 201
+
+
+api.add_resource(ProductCollection, "/api/products/")
+
+
+class StorageItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
+    qty = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(64), nullable=False)
+
+    product = db.relationship("Product", back_populates="in_storage")
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    handle = db.Column(db.String(64), unique=True, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    in_storage = db.relationship("StorageItem", back_populates="product")
+
+db.create_all()
